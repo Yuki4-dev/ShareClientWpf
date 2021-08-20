0c4516d2afb23b920c253e5e48aa9cf39403f50a
LAPTOP-ITBNKE7K\sanak <cdg.akfg@gmail.com>
2021-08-20 12:52:14 +0900

    commit

---
+++ ShareClientWpf/ViewModel/SendWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShareClientWpf
+{
+    public class SendWindowViewModel : ViewModelBase
+    {
+        private Action<object> callback;
+
+        private string ipText;
+        public string IptText
+        {
+            get => ipText;
+            set => SetProperty(ref ipText, value);
+        }
+
+        private string portText;
+        public string PortText
+        {
+            get => portText;
+            set => SetProperty(ref portText, value);
+        }
+
+        private string message;
+        public string Message
+        {
+            get => message;
+            set => SetProperty(ref message, value);
+        }
+
+        private ICommand sendCommand;
+        public ICommand SendCommand
+        {
+            get => sendCommand;
+            set => SetProperty(ref sendCommand, value);
+        }
+
+
+        public SendWindowViewModel()
+        {
+            SendCommand = new Command(SendExecute);
+        }
+
+        public override void LoadedProcces(object paramater, Action<object> executeCallback)
+        {
+            callback = executeCallback;
+        }
+
+        public void SendExecute()
+        {
+            if (int.TryParse(IptText, out var ip) && int.TryParse(PortText, out var port))
+            {
+                callback?.Invoke(Tuple.Create(ip, port));
+                OnCloseWindow();
+            }
+            else
+            {
+                Message = "※数字を入力してください。";
+            }
+        }
+
+    }
+}
